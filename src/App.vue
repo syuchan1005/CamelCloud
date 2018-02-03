11753a63514bee47501ac2eb454f8ae35f8512f3
syuchan1005 <syuchan.dev@gmail.com>
2018-02-04 00:37:53 +0900

    Add Center menu

--- src/App.vue
+++ src/App.vue
@@ -8,61 +8,48 @@
       </md-button>
     </div>
 
-    <md-list class="side" :data-side="list" v-if="!under480 || !$store.state.viewFilter">
-      <md-list-item @click="$store.commit('viewFilter', 'all')" :disabled="$store.state.viewFilter === 'all'">
-        <md-icon>photo_library</md-icon>
-        <span>Camera Roll</span>
-      </md-list-item>
+    <md-list class="side" :data-side="list" v-show="!under480 || !$store.state.viewFilter">
+      <div v-for="(filter, index) in filters" :key="index">
+        <md-list-item @click="$store.commit('viewFilter', filter.name)"
+                      :disabled="$store.state.viewFilter === filter.name">
+          <md-icon>{{ filter.icon }}</md-icon>
+          <span>{{ filter.label }}</span>
+        </md-list-item>
 
-      <md-divider class="mobile"/>
-
-      <md-list-item @click="$store.commit('viewFilter', 'photo')" :disabled="$store.state.viewFilter === 'photo'">
-        <md-icon>photo</md-icon>
-        <span>Photos</span>
-      </md-list-item>
-
-      <md-divider class="mobile"/>
-
-      <md-list-item @click="$store.commit('viewFilter', 'video')" :disabled="$store.state.viewFilter === 'video'">
-        <md-icon>video_label</md-icon>
-        <span>Videos</span>
-      </md-list-item>
-
-      <md-divider class="mobile"/>
-
-      <md-list-item @click="$store.commit('viewFilter', 'favorite')" :disabled="$store.state.viewFilter === 'favorite'">
-        <md-icon>star</md-icon>
-        <span>Favorites</span>
-      </md-list-item>
-
-      <md-divider class="mobile"/>
-
-      <md-list-item @click="$store.commit('viewFilter', 'delete')" :disabled="$store.state.viewFilter === 'delete'">
-        <md-icon>delete</md-icon>
-        <span>Recently Deleted</span>
-      </md-list-item>
-
-      <md-list-item class="side-select">
-        <div>Menu</div>
-        <md-button-toggle md-single class="md-primary">
-          <md-button class="md-toggle md-icon-button" @click="list = 'left'">
-            <md-icon>keyboard_arrow_left</md-icon>
-          </md-button>
-          <md-button class="md-icon-button" @click="list = 'right'">
-            <md-icon>keyboard_arrow_right</md-icon>
-          </md-button>
-        </md-button-toggle>
-      </md-list-item>
+        <md-divider class="mobile" v-show="filters.length - 1 !== index"/>
+      </div>
     </md-list>
+
+    <div class="side-select" v-show="!under800">
+      <div>Menu</div>
+      <md-button-toggle md-single class="md-primary">
+        <md-button class="md-icon-button" :class="{ 'md-toggle': list === 'left' }" @click="listSide = 'left'">
+          <md-icon>keyboard_arrow_left</md-icon>
+        </md-button>
+        <md-button class="md-icon-button" :class="{ 'md-toggle': list === 'center' }" @click="listSide = 'center'">
+          <md-icon>keyboard_arrow_down</md-icon>
+        </md-button>
+        <md-button class="md-icon-button" :class="{ 'md-toggle': list === 'right' }" @click="listSide = 'right'">
+          <md-icon>keyboard_arrow_right</md-icon>
+        </md-button>
+      </md-button-toggle>
+    </div>
 
     <md-button class="md-icon-button" v-show="under480 && $store.state.viewFilter"
                @click="$store.commit('viewFilter', undefined)">
       <md-icon>arrow_back</md-icon>
     </md-button>
 
-    <main class="always" v-if="!under480 || $route.path === '/' || (under480 && $store.state.viewFilter)">
+    <main class="always" :class="{ [list] : !noMenu }"
+          v-show="!under480 || noMenu || (under480 && $store.state.viewFilter)">
       <router-view/>
     </main>
+
+    <md-tabs md-centered class="md-transparent" data-side="center" v-show="list === 'center'" @change="(index) => $store.commit('viewFilter', filters[index].name)">
+      <md-tab v-for="(filter, index) in filters" :key="index"
+              :md-active="$store.state.viewFilter === filter.name"
+              :md-label="filter.label" :md-icon="filter.icon" />
+    </md-tabs>
 
     <vue-snotify class="always"></vue-snotify>
   </div>
@@ -73,8 +60,36 @@
     name: 'app',
     data() {
       return {
-        list: 'left',
+        listSide: 'left',
+        filters: [
+          {
+            label: 'Camera Roll',
+            icon: 'photo_library',
+            name: 'all',
+          },
+          {
+            label: 'Photos',
+            icon: 'photo',
+            name: 'photo',
+          },
+          {
+            label: 'Videos',
+            icon: 'video_label',
+            name: 'video',
+          },
+          {
+            label: 'Favorite',
+            icon: 'star',
+            name: 'favorite',
+          },
+          {
+            label: 'Recently Deleted',
+            icon: 'delete',
+            name: 'delete',
+          },
+        ],
         innerWidth: window.innerWidth,
+        tabChange: undefined,
       };
     },
     mounted() {
@@ -84,8 +99,22 @@
       window.removeEventListener('resize', this.handleResize);
     },
     computed: {
+      list() {
+        if (this.under480) {
+          return 'left';
+        } else if (this.under800) {
+          return 'center';
+        }
+        return this.listSide;
+      },
       under480() {
         return this.innerWidth <= 480;
+      },
+      under800() {
+        return this.innerWidth <= 800;
+      },
+      noMenu() {
+        return ['/', '/login'].includes(this.$route.path);
       },
     },
     methods: {
@@ -94,6 +123,7 @@
       },
       clickLogout() {
         this.$store.commit('token', undefined);
+        this.$store.commit('viewFilter', undefined);
         this.$router.push({ path: '/' });
       },
     },
@@ -157,7 +187,20 @@
   }
 
   .side-select {
-    margin-top: auto;
+    z-index: 2;
+
+    position: absolute;
+    bottom: 10px;
+    display: flex;
+    align-items: center;
+
+    & > div {
+      margin-left: 10px;
+    }
+  }
+
+  .side[data-side="right"] + .side-select {
+    right: 10px;
   }
 
   .side {
@@ -174,8 +217,33 @@
     border-left: solid $dividerColor 1px;
   }
 
+  .side[data-side="center"] {
+    display: none;
+  }
+
+  main.center {
+    width: 100%;
+    height: calc(100% - 3rem - 72px);
+  }
+
   .md-list-item.md-disabled {
     background: rgba(0, 0, 0, 0.1);
+
+    &  > .md-button > * {
+      color: #3F51B5;
+    }
+  }
+
+  .md-tab-header > div {
+    text-transform: none;
+  }
+
+  .md-tabs {
+    height: 72px;
+  }
+
+  .md-tabs-content {
+    display: none;
   }
 
   @media all and (max-width: 480px) {
