cddcccc2875071a59cb1e48547d5efde77fad554
syuchan1005 <syuchan.dev@gmail.com>
2018-02-14 16:28:10 +0900

    :sparkles: Add koa-multer

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,3 +1,4 @@
+import fs from 'fs-extra';
 import opn from 'opn';
 import os from 'os';
 import Koa from 'koa';
@@ -6,6 +7,7 @@ import Session from 'koa-session';
 import Router from 'koa-router';
 import Serve from 'koa-static';
 import koaPassport from 'koa-passport';
+import multer from 'koa-multer';
 import historyApiFallback from 'koa2-connect-history-api-fallback';
 import Passport from './Passport.mjs';
 import GraphQL from './GraphQL.mjs';
@@ -23,6 +25,20 @@ app.use(koaPassport.initialize());
 app.use(koaPassport.session());
 
 const db = new DBManager();
+
+const uploadStorage = multer.diskStorage({
+  async destination(request, file, callback) {
+    const user = await db.getUser(request.user.userId);
+    const path = `../Storage/${user.dirName}/${request.body.path || ''}`;
+    await fs.ensureDir(path);
+    callback(null, path);
+  },
+  async filename(request, file, callback) {
+    callback(null, file.originalname);
+  },
+});
+const upload = multer({ storage: uploadStorage });
+
 const graphQL = new GraphQL(db);
 const passport = new Passport(db);
 
@@ -32,7 +48,7 @@ apiRouter.all('/', async (ctx, next) => {
   if (ctx.isAuthenticated()) {
     await next();
   } else {
-    ctx.status = 401;
+    ctx.throw(401);
   }
 }, graphQL.http());
 
@@ -50,6 +66,16 @@ apiRouter.use('/auth', authRouter.routes(), authRouter.allowedMethods());
 apiRouter.get('/logout', async (ctx) => {
   await ctx.logout();
   ctx.redirect('/');
+});
+
+apiRouter.post('/upload', async (ctx, next) => {
+  if (ctx.isAuthenticated()) {
+    await next();
+  } else {
+    ctx.throw(401);
+  }
+}, upload.single('photo'), async (ctx) => {
+  ctx.status = 200;
 });
 
 const router = new Router();
