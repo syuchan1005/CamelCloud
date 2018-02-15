4248087c2883b47764990d1b249ef5f345c4f4b6
syuchan1005 <syuchan.dev@gmail.com>
2018-02-15 09:14:58 +0900

    :sparkles: Add File Operation menus

--- Client/src/components/CameraRoll.vue
+++ Client/src/components/CameraRoll.vue
@@ -5,9 +5,24 @@
       <div class="title">Add your first photo</div>
     </div>
     <div v-else class="path">{{ path }}</div>
-    <vue-perfect-scrollbar class="files">
-      <file @click="fileClick(file)" v-for="(file, index) in files" :key="index" :name="file.name" :type="file.type" />
+    <vue-perfect-scrollbar class="files" @contextmenu.native.prevent="click($event)">
+      <file @click="fileClick(file)" v-for="(file, index) in files" :key="index"
+            :name="file.name" :type="file.type" @rename="renameFile(file)" @remove="removeFile(file)"/>
     </vue-perfect-scrollbar>
+
+    <md-menu md-size="4" ref="menu" :style="menu">
+      <div md-menu-trigger></div>
+
+      <md-menu-content>
+        <md-menu-item @selected="openNewDir">
+          <md-icon>add</md-icon>
+          <span>Create New Folder</span>
+        </md-menu-item>
+      </md-menu-content>
+    </md-menu>
+
+    <md-dialog-prompt :md-title="dialog.title" :md-input-placeholder="dialog.placeholder"
+                      :md-ok-text="dialog.okText" @close="closeDialog" v-model="dialog.value" ref="dialog"/>
   </div>
 </template>
 
@@ -26,6 +41,18 @@
       return {
         path: '> ',
         files: [],
+        menu: {
+          top: '0px',
+          left: '0px',
+        },
+        dialog: {
+          op: '',
+          title: '',
+          placeholder: '',
+          okText: '',
+          value: '',
+          file: undefined,
+        },
       };
     },
     mounted() {
@@ -53,12 +80,72 @@
           this.path += `${file.name} > `;
         }
       },
+      click(event) {
+        this.menu.top = `${event.clientY}px`;
+        this.menu.left = `${event.clientX}px`;
+        setTimeout(this.$refs.menu.open, 50);
+      },
+      closeDialog(state) {
+        if (state !== 'ok') return;
+        const input = {
+          op: this.dialog.op,
+          path: this.path.replace(/> /g, '/'),
+          source: this.dialog.value,
+        };
+        if (input.op === 'RENAME') {
+          input.source = this.dialog.file;
+          input.target = this.dialog.value;
+        }
+        this.$http({
+          method: 'post',
+          url: '/api',
+          data: {
+            query: 'mutation OperateFile($input: opFile){operateFile(data:$input){type name}}',
+            variables: { input },
+          },
+        }).then((response) => {
+          this.files = response.data.data.operateFile;
+        });
+      },
+      renameFile(file) {
+        this.dialog = {
+          op: 'RENAME',
+          title: 'Rename',
+          placeholder: 'Name',
+          okText: 'Rename',
+          value: file.name,
+          file: file.name,
+        };
+        this.$refs.dialog.open();
+      },
+      removeFile(file) {
+        this.dialog = {
+          op: 'REMOVE',
+          value: file.name,
+        };
+        this.closeDialog('ok');
+      },
+      openNewDir() {
+        this.dialog = {
+          op: 'MKDIR',
+          title: 'New Directory',
+          placeholder: 'DirectoryName',
+          okText: 'Create',
+          value: '',
+          file: undefined,
+        };
+        this.$refs.dialog.open();
+      },
     },
   };
 </script>
 
 <style lang="scss" scoped>
   @import 'general';
+
+  .md-menu {
+    position: absolute;
+  }
 
   .select-mode {
     height: 100%;
